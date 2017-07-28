67132c98041abd86a1ac1e9291d8ad47f68f31b1
E Yiu <elnry2@gmail.com>
2017-07-28 01:48:17 +0000

    Added code comments for Stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  # Whitelist the follwing form fields so that we can process them, if coming from
+  #a devise controller
   before_action :configure_premitted_paramters, if: :devise_controller?
   
   protected
-      def configure_premitted_paramters
-          devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:stripe_card_token, :email, :password, :password_confirmation)}
-      end
+    def configure_premitted_paramters
+        devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:stripe_card_token, :email, :password, :password_confirmation)}
+    end
 end
